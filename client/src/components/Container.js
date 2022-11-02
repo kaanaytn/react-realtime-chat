183d33f2be29360ddf0c2a59eeb9f9e8580db833
kaan <kaanayten@hotmail.com>
2022-11-03 01:15:50 +0300

    chat list client upgrade

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,9 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 
 import ChatList from "./ChatList";
 import ChatForm from "./ChatForm";
+import { useChat } from "../context/ChatContext";
+
+import { init, subscribeChat } from "../socketApi";
 
 function Container() {
+  const { setMessages } = useChat();
+
+  useEffect(() => {
+    init();
+    subscribeChat((message) => {
+      setMessages((prevState) => [...prevState, { message }]);
+    });
+  }, []);
+
   return (
     <div className='App'>
       <ChatList />
