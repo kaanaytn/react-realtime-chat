7d644de557e55b7bb676a5588a9e77434dd0d671
kaan <kaanayten@hotmail.com>
2022-11-03 13:15:40 +0300

    project complete

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -2,17 +2,18 @@ import styles from "./styles.module.css";
 
 import { useChat } from "../context/ChatContext";
 import ChatItem from "./ChatItem";
+import ScrollableFeed from "react-scrollable-feed";
 
 const ChatList = () => {
   const { messages } = useChat();
 
   return (
     <div className={styles.chatlist}>
-      <div>
+      <ScrollableFeed forceScroll={true} className='scrollbar'>
         {messages.map((item, key) => (
           <ChatItem key={key} item={item} />
         ))}
-      </div>
+      </ScrollableFeed>
     </div>
   );
 };
